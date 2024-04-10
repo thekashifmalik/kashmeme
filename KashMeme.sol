08c041c19becb043c0abd5b03e229ab9fa6340dc
thekashifmalik <emailkashifmalik@gmail.com>
2024-04-09 20:10:44 -0700

    Use etherws

--- KashMeme.sol
+++ KashMeme.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KashMeme is ERC20 {
     constructor() ERC20("KashMeme", "KASHMEME") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 100 * 10 ** decimals());
     }
 }
